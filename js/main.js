0ace16e650c23bb15c3b39b788a4c0156718b164
Anna Kviese <anna.kviese@gmail.com>
2017-05-05 17:12:23 -0700

    updated mobile styles

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 $(function () {
+    $(window).on("resize", function (e) {
+        checkScreenSize();
+    });
     $('.large-post.post, .small-post.post').slice(0, 3).css('display','flex');
+    // $('.large-post.post, .small-post.post').slice(0, 3).show();
     $("#loadMore").on('click', function (e) {
         e.preventDefault();
+
         $(".large-post.post, .small-post.post:hidden").slice(0, 4).slideDown().css('display','flex');
+        
         if ($(".large-post.post, .small-post.post:hidden:hidden").length == 0) {
             $("#load").fadeOut('slow');
         }
@@ -11,3 +17,10 @@ $(function () {
         }, 1500);
     });
 });
+
+
+
+    // checkScreenSize();
+    // function checkScreenSize(){
+    //     var newWindowWidth = $(window).width()
+    //     if (newWindowWidth < 600) {
