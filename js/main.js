b0281810241a8494a152d0658c7e075a802c40f9
Anna Kviese <anna.kviese@gmail.com>
2017-05-07 19:48:13 -0700

    updated button show hide functions

--- js/main.js
+++ js/main.js
@@ -2,18 +2,17 @@ $(function () {
     $('.large-post.post, .small-post.post').slice(0, 3).css('display','flex'); //show first 3 event posts on load 
     $("#loadMore").on('click', function (e) { // on click of Load More button 
         e.preventDefault();
-        var postsHidden = $('.large-post.post, .small-post.post:hidden');   
-        $(postsHidden).slice(0, 4).slideDown().css('display','flex');   //slide Down 3 more posts 
-        
-        if ($(postsHidden).length < 0) {
-            $('#loadMore').hide();
-        }
-        else {
-            $('#loadMore').show();
-        }
+        $('.large-post.post:hidden, .small-post.post:hidden').slice(0, 4).slideDown().css('display','flex');   //slide Down 3 more posts 
         $('html,body').animate({
             scrollTop: $(this).offset().top
         }, 1500);
+
+        // check if there are any hidden posts left
+        if ($('.large-post.post:hidden, .small-post.post:hidden').length == 0) {
+            $('#loadMore').hide();
+            $('#backToTop').css('display','flex');
+        }
+        
 
         //Scrolls back to top on click of Back To Top button 
         $('#backToTop').click(function () {
