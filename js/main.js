e7287034e8fa4f82bff4bda84aa3bc6419a5a4c5
Anna Kviese <anna.kviese@gmail.com>
2017-05-07 19:29:12 -0700

    added back to top button

--- js/main.js
+++ js/main.js
@@ -1,15 +1,26 @@
 $(function () {
-    $('.large-post.post, .small-post.post').slice(0, 3).css('display','flex');
-    $("#loadMore").on('click', function (e) {
+    $('.large-post.post, .small-post.post').slice(0, 3).css('display','flex'); //show first 3 event posts on load 
+    $("#loadMore").on('click', function (e) { // on click of Load More button 
         e.preventDefault();
-        var postsHidden = $('.large-post.post, .small-post.post:hidden');
-        $(postsHidden).slice(0, 4).slideDown().css('display','flex');
+        var postsHidden = $('.large-post.post, .small-post.post:hidden');   
+        $(postsHidden).slice(0, 4).slideDown().css('display','flex');   //slide Down 3 more posts 
         
-        if ($(postsHidden).length == 0) {
-            $("#load").fadeOut('slow');
+        if ($(postsHidden).length < 0) {
+            $('#loadMore').hide();
+        }
+        else {
+            $('#loadMore').show();
         }
         $('html,body').animate({
             scrollTop: $(this).offset().top
         }, 1500);
+
+        //Scrolls back to top on click of Back To Top button 
+        $('#backToTop').click(function () {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 600);
+            return false;
+        });
     });
 });
